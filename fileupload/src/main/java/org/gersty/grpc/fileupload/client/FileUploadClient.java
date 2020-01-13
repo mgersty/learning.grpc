a0ab0edcb6ad2d32de3589dc27d3ed7056dd04e0
Matthew P Gerstemeier mpg26 <mgerstemeier@cas.org>
2020-01-13 16:40:09 -0500

    adding in proper logic to do unary/one direction/bidirectional streaming

--- fileupload/src/main/java/org/gersty/grpc/fileupload/client/FileUploadClient.java
+++ fileupload/src/main/java/org/gersty/grpc/fileupload/client/FileUploadClient.java
@@ -1,11 +1,12 @@
 package org.gersty.grpc.fileupload.client;
 
 import com.google.protobuf.ByteString;
+import com.sun.corba.se.pept.transport.ResponseWaitingRoom;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
-import org.gersty.grpc.standalone.fileupload.FileRequest;
-import org.gersty.grpc.standalone.fileupload.FileResponse;
+import org.gersty.grpc.standalone.fileupload.Request;
+import org.gersty.grpc.standalone.fileupload.Response;
 import org.gersty.grpc.standalone.fileupload.FileUploadServiceGrpc;
 
 
@@ -18,9 +19,11 @@ import java.util.logging.Logger;
 public class FileUploadClient {
     private static final Logger logger = Logger.getLogger(FileUploadClient.class.getName());
     private static final int PORT = 50051;
+    public static final String test_file = "test.txt";
 
     private final ManagedChannel mChannel;
     private final FileUploadServiceGrpc.FileUploadServiceStub mAsyncStub;
+    private final FileUploadServiceGrpc.FileUploadServiceBlockingStub blockingStub;
 
     public FileUploadClient(String host, int port) {
         this(ManagedChannelBuilder.forAddress(host, port)
@@ -33,6 +36,7 @@ public class FileUploadClient {
     FileUploadClient(ManagedChannel channel) {
         this.mChannel = channel;
         mAsyncStub = FileUploadServiceGrpc.newStub(channel);
+        blockingStub = FileUploadServiceGrpc.newBlockingStub(channel);
     }
 
     public void shutdown() throws InterruptedException {
@@ -40,14 +44,19 @@ public class FileUploadClient {
     }
 
 
+    public void startSingle(){
+        Response resp = blockingStub.single(Request.newBuilder().setValue("Hello from the client").build());
+        logger.info(resp.getMessage()+" "+resp.getStatus());
+    }
+
     public void startStream(final String filepath) throws FileNotFoundException {
         logger.info("tid: " + Thread.currentThread().getId() + ", Will try to getBlob");
 
-        StreamObserver<FileResponse> responseObserver = new StreamObserver<FileResponse>() {
+        StreamObserver<Response> responseObserver = new StreamObserver<Response>() {
 
             @Override
-            public void onNext(FileResponse value) {
-                logger.info("Client response onNext");
+            public void onNext(Response value) {
+                logger.info("****** Final Response From Client ****** "+value.getStatus());
             }
 
             @Override
@@ -61,17 +70,17 @@ public class FileUploadClient {
             }
         };
 
-        StreamObserver<FileRequest> requestObserver = mAsyncStub.upload(responseObserver);
+        StreamObserver<Request> requestObserver = mAsyncStub.upload(responseObserver);
 
         try {
-            BufferedInputStream bInputStream = new BufferedInputStream(new FileInputStream(getClass().getClassLoader().getResource("test.txt").getFile()));
+            BufferedInputStream bInputStream = new BufferedInputStream(new FileInputStream(getClass().getClassLoader().getResource(filepath).getFile()));
 
-            int bufferSize = 1 * 1024; // 512k
+            int bufferSize = 1 * 1024;
             byte[] buffer = new byte[bufferSize];
             int size = 0;
             while ((size = bInputStream.read(buffer)) > 0) {
                 ByteString byteString = ByteString.copyFrom(buffer, 0, size);
-                FileRequest req = FileRequest.newBuilder().setName(filepath).setData(byteString).build();
+                Request req = Request.newBuilder().setValue(filepath).setData(byteString).build();
                 requestObserver.onNext(req);
             }
         } catch (FileNotFoundException e) {
@@ -82,14 +91,83 @@ public class FileUploadClient {
         requestObserver.onCompleted();
 }
 
+    public void startDownload(){
+        StreamObserver<Response> responseObserver = new StreamObserver<Response>() {
+
+            @Override
+            public void onNext(Response value) {
+
+                logger.info("****** Responses From Server ****** "+value.getMessage());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                logger.info("Client response onError");
+            }
+
+            @Override
+            public void onCompleted() {
+                logger.info("Server has completed sending messages to client");
+            }
+        };
+
+        Request request = Request.newBuilder().setValue("w").build();
+        mAsyncStub.download(request, responseObserver);
+
+
+    }
+
+    public void startBidirectional(final String filepath) throws FileNotFoundException {
+        StreamObserver<Response> responseObserver = new StreamObserver<Response>() {
+
+            @Override
+            public void onNext(Response value) {
+                logger.info("\n****** Response from server ******\n"+value.getMessage()+"\n"+value.getStatus());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                logger.info("Client response onError");
+            }
+
+            @Override
+            public void onCompleted() {
+                logger.info("****** All Messages sent from client ****** ");
+            }
+        };
+
+        StreamObserver<Request> requestObserver = mAsyncStub.bidirectional(responseObserver);
+
+        try {
+            BufferedInputStream bInputStream = new BufferedInputStream(new FileInputStream(getClass().getClassLoader().getResource(filepath).getFile()));
+
+            int bufferSize = 1 * 1024;
+            byte[] buffer = new byte[bufferSize];
+            int size = 0;
+            while ((size = bInputStream.read(buffer)) > 0) {
+                ByteString byteString = ByteString.copyFrom(buffer, 0, size);
+                Request req = Request.newBuilder().setValue(filepath).setData(byteString).build();
+                requestObserver.onNext(req);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        requestObserver.onCompleted();
+    }
+
+
     public static void main(String[] args) throws Exception {
         FileUploadClient client = new FileUploadClient("localhost", PORT);
         try {
-            client.startStream("airplane_sky_flight_clouds.jpg");
-            logger.info("Done with startStream");
+ //           client.startSingle();
+  //          client.startStream(test_file);
+ //           client.startDownload();
+            client.startBidirectional(test_file);
         } finally {
             try {
-                Thread.sleep(500);
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- fileupload/src/main/java/org/gersty/grpc/fileupload/server/FileUploadService.java
+++ fileupload/src/main/java/org/gersty/grpc/fileupload/server/FileUploadService.java
@@ -1,44 +1,204 @@
 package org.gersty.grpc.fileupload.server;
 
-import com.google.protobuf.ByteString;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
-import org.gersty.grpc.standalone.fileupload.FileRequest;
+import org.gersty.grpc.standalone.fileupload.Request;
 import org.gersty.grpc.standalone.fileupload.FileUploadServiceGrpc;
-import org.gersty.grpc.standalone.fileupload.FileResponse;
+import org.gersty.grpc.standalone.fileupload.Response;
 
 import java.io.BufferedOutputStream;
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 class FileUploadService extends FileUploadServiceGrpc.FileUploadServiceImplBase {
     private int mStatus = 200;
     private String mMessage = "";
     private BufferedOutputStream mBufferedOutputStream = null;
+    private static final Logger logger = Logger.getLogger(FileUploadService.class.getName());
 
     @Override
-    public StreamObserver<FileRequest> upload(final StreamObserver<FileResponse> responseObserver){
+    public void single(Request request, final StreamObserver<Response> response){
+        logger.info("Message from client: "+request.getValue());
+        response.onNext(Response.newBuilder().setStatus("Success").setMessage("Message from Server").build());
+        response.onCompleted();
+    }
 
-        return new StreamObserver<FileRequest>() {
+    @Override
+    public StreamObserver<Request> upload(final StreamObserver<Response> responseObserver) {
+        logger.info("****** Initialize Stream from Client to Server ****** ");
+        return new StreamObserver<Request>() {
             @Override
-            public void onNext(FileRequest file) {
-                ByteString stuff = file.getData();
-                System.out.println("-- STREAM FROM CLIENT -->"+new String(stuff.toByteArray()));
-                responseObserver.onNext(FileResponse.newBuilder().setStatus("success").build());
+            public void onNext(Request file) {
+                logger.info("****** Data from client ******\n" + new String(file.getData().toByteArray()));
             }
 
             @Override
             public void onError(Throwable throwable) {
-                System.out.println(throwable.getMessage());
+                logger.info(throwable.getMessage());
                 throwable.printStackTrace();
             }
 
             @Override
             public void onCompleted() {
-                responseObserver.onNext(FileResponse.newBuilder().setStatus("Final Success").build());
+                responseObserver.onNext(Response.newBuilder().setStatus("All messages received").build());
                 responseObserver.onCompleted();
             }
         };
     }
+
+    @Override
+    public void download(Request fileRequest, final StreamObserver<Response> responseObserver) {
+        logger.info("****** Initialize Stream from Sever to Client ****** ");
+
+        logger.info("****** Filter Names Containing Character****** " + fileRequest.getValue());
+
+        names().stream()
+                .map(String::toLowerCase)
+                .filter(name-> name.contains(fileRequest.getValue().toLowerCase()))
+                .collect(Collectors.toList())
+            .forEach(name -> responseObserver.onNext(Response.newBuilder().setMessage(name).build()));
+
+        responseObserver.onCompleted();
+
+    }
+    int mmCount = 0;
+
+    @Override
+    public StreamObserver<Request> bidirectional(final StreamObserver<Response> responseObserver) {
+        logger.info("****** Initialize BiDi Stream for Client & Server ****** ");
+
+        return new StreamObserver<Request>() {
+            @Override
+            public void onNext(Request file) {
+                logger.info("sync counter: " + mmCount +"\n"+"data received from client: " + new String(file.getData().toByteArray()));
+
+                String message = ((new Random().nextInt(10) & 2) == 0) ? "message received":"message not received";
+                responseObserver.onNext(Response.newBuilder().setMessage("sync counter: " + mmCount).setStatus(message+": "+new String(file.getData().toByteArray())).build());
+                mmCount++;
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                logger.info(throwable.getMessage());
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(Response.newBuilder().setStatus("All messages acknowledge").build());
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+
+
+
+    private static List<String> names() {
+        return Arrays.asList(
+                "Sophia",
+                "Jackson",
+                "Emma",
+                "Aiden",
+                "Olivia",
+                "Lucas",
+                "Ava",
+                "Liam",
+                "Mia",
+                "Noah",
+                "Isabella",
+                "Ethan",
+                "Riley",
+                "Mason",
+                "Aria",
+                "Caden",
+                "Zoe",
+                "Oliver",
+                "Charlotte",
+                "Elijah",
+                "Lily",
+                "Grayson",
+                "Layla",
+                "Jacob",
+                "Amelia",
+                "Michael",
+                "Emily",
+                "Benjamin",
+                "Madelyn",
+                "Carter",
+                "Aubrey",
+                "James",
+                "Adalyn",
+                "Jayden",
+                "Madison",
+                "Logan",
+                "Chloe",
+                "Alexander",
+                "Harper",
+                "Caleb",
+                "Abigail",
+                "Ryan",
+                "Aaliyah",
+                "Luke",
+                "Avery",
+                "Daniel",
+                "Evelyn",
+                "Jack",
+                "Kaylee",
+                "William",
+                "Ella",
+                "Owen",
+                "Ellie",
+                "Gabriel",
+                "Scarlett",
+                "Matthew",
+                "Arianna",
+                "Connor",
+                "Hailey",
+                "Jayce",
+                "Nora",
+                "Isaac",
+                "Addison",
+                "Sebastian",
+                "Brooklyn",
+                "Henry",
+                "Hannah",
+                "Muhammad",
+                "Mila",
+                "Cameron",
+                "Leah",
+                "Wyatt",
+                "Elizabeth",
+                "Dylan",
+                "Sarah",
+                "Nathan",
+                "Eliana",
+                "Nicholas",
+                "Mackenzie",
+                "Julian",
+                "Peyton",
+                "Eli",
+                "Maria",
+                "Levi",
+                "Grace",
+                "Isaiah",
+                "Adeline",
+                "Landon",
+                "Elena",
+                "David",
+                "Anna",
+                "Christian",
+                "Victoria",
+                "Andrew",
+                "Camilla",
+                "Brayden",
+                "Lillian",
+                "John",
+                "Natalie",
+                "Lincoln"
+        );
+    }
+
 }
